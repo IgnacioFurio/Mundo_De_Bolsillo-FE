34dd5da6fd3a371825054014b4365872f5fc9e39
Ignacio Furió José <bichoifj@gmail.com>
2024-01-23 18:26:01 +0100

    Instaled redux, redux thunk and redux persistor to my app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from './App'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+//redux
+import { Provider } from 'react-redux';
+import store from './app/store'
+// redux-persist 
+import { PersistGate } from 'redux-persist/integration/react'
+import persistStore from 'redux-persist/es/persistStore'
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App/>
+        </BrowserRouter>  
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
