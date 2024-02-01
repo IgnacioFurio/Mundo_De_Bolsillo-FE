890b23427935ecfab0aaf5f9f40122c2a1a851a7
Ignacio Furió José <bichoifj@gmail.com>
2024-02-01 11:55:35 +0100

    chore: NewRegisterButton design Css

--- src/common/NewRegisterButton/NewRegisterButton.jsx
+++ src/common/NewRegisterButton/NewRegisterButton.jsx
@@ -8,8 +8,10 @@ import "./NewRegisterButton.css";
 export const NewRegisterButton = ({name, clickFunction}) => {
 
     return (
-        <div className='NewRegisterButton fw-bold  p-2' onClick={clickFunction}>
-            {name}
-        </div>
+        <Container className='d-flex justify-content-center'>
+            <div className='NewRegisterButton d-flex align-items-center justify-content-center fw-bold' onClick={clickFunction}>
+                <p className='burnedText fs-5 p-4 pb-3'>{name}</p>
+            </div>
+        </Container>
     )
 }
