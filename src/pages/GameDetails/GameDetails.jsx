b2860ac3620263305e5172e1544cfc19938e00bc
Ignacio Furió José <bichoifj@gmail.com>
2024-02-04 15:20:18 +0100

    created a portrait for the game title

--- src/pages/GameDetails/GameDetails.jsx
+++ src/pages/GameDetails/GameDetails.jsx
@@ -8,7 +8,6 @@ import { deleteGame } from '../../services/game.apicalls';
 import { BackButton } from '../../common/BackButton/BackButton';
 import { ModifyButton } from '../../common/ModifyButton/ModifyButton';
 import { DeleteButton } from '../../common/DeleteButton/DeleteButton';
-import { PageNavigator } from '../../common/PageNavigator/PageNavigator';
 //bootstrap
 import { Container, Row , Col} from 'react-bootstrap';
 //css
@@ -40,11 +39,13 @@ export const GameDetails = () => {
                 <Col className='col-4 d-flex justify-content-center my-2'><ModifyButton clickFunction={() => navigate("/games/modify-game")}/></Col>
                 <Col className='col-4 d-flex justify-content-center my-2'><DeleteButton gameData={gameInformation} clickFunction={() => deleteGameData(gameInformation.id)}/></Col>
             </Row>            
-            <Row className='gameDetailsBackground mt-3 p-3'>
-                <Col className='gameTitle col-12 d-flex justify-content-center fs-4 fw-bold'>
-                    {gameInformation.title}
-                </Col>
-                <Col className='gameDescription col-12 text-center mt-3 mx-2'>
+            <Row className='gameDetailsStone pt-1'>
+                <Col className='gamePortraitTitle p-2'>
+                    <p className='gameDetailsTitle d-flex justify-content-center col-12 fs-4 fw-bold'>{gameInformation.title}</p>
+                </Col>                    
+            </Row>
+            <Row className='gameDetailsBackground'>
+                <Col className='gameDetailsDescription col-12 text-center mb-3 mx-2'>
                 {gameInformation.description}
                 </Col>
             </Row>
