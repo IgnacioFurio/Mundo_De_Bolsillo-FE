c1e8c68cf8cbec2fdcca34444f3bf9667b127be3
Ignacio Furió José <bichoifj@gmail.com>
2024-03-12 17:19:29 +0100

    chore: minor change to a prompt at the game creation

--- src/helpers/Games.Forms.helper.js
+++ src/helpers/Games.Forms.helper.js
@@ -7,7 +7,7 @@ export const GameFormQuestions = {
         modify: {
             title: "¿Has encontrado un nombre aún más molón?, recuerda que siempre puedes volver a cambiarlo.",
             description: "Si hay algo que quieras añadir sobre la partida este es buen momento.",
-            worldgate: "Selecciona que portales quieres mantener abiertos y cuales cerrados para esta partida."
+            worldgate: "Selecciona que portales quieres mantener abiertos y cuales mantener cerrados para esta partida."
         }
     },
     placeholder: {
