20f49f5210568c6c4794a4e7d07a393108d04d50
Ignacio Furió José <bichoifj@gmail.com>
2024-07-02 18:43:15 +0200

    created basic design for the tutorial

--- src/pages/NewCharacter/NewCharacter.jsx
+++ src/pages/NewCharacter/NewCharacter.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import { TutorialQuestions } from '../../common/TutorialQuestions/TutorialQuestions';
+import { ConfirmNewRegister } from '../../common/confirmNewRegister/confirmNewRegister';
+import { NextPrevButton } from '../../common/NextPrevButton/NextPrevButton';
 
 export const NewCharacter = () => {
     const navigate = useNavigate();
@@ -37,6 +41,45 @@ export const NewCharacter = () => {
 
 
     return (
-        <div>NewCharacter</div>
+        <Container>
+            <Row className='tutorialHeight'>
+            {formCounter === 0 && <TutorialQuestions
+                gameData={newCharacterData.name}
+                type="textarea" 
+                text={formQuestions.name}
+                errorText={errorInputField.nameError}
+                placeholder={formPlaceholders.name} 
+                name="name" 
+                required={true}
+                changeFunction={(e) => inputHandler(e)}
+                blurFunction={(e)=>checkError(e)}/>
+            }
+            
+            
+            {formCounter === 8 && <ConfirmNewRegister data={newCharacterData}/>}
+            </Row>
+
+            <Row className='nextPrev d-flex justify-content-center align-items-center'>
+            {formCounter < 8 ?  //Sección para los botones de avanzar/retroceder o enviar información en los formularios
+                <>
+                    <Col className='d-flex justify-content-start'>
+                        <NextPrevButton action="Prev" clickFunction={() => formHandlerPrev()}/>
+                    </Col>
+                    <Col className='d-flex justify-content-end'>
+                    {submitStatus === true ? <NextPrevButton action="Next" clickFunction={() => formHandlerNext()}/> : <NextPrevButton action="Wait" clickFunction={() => {}}/>}
+                    </Col>  
+                </>
+                :
+                <>
+                    <Col className='d-flex justify-content-start'>
+                        <NextPrevButton action="Prev" clickFunction={() => formHandlerPrev()}/>
+                    </Col>
+                    <Col className='d-flex justify-content-end'>
+                        <NextPrevButton gameInfo={newLocationData} action="Submit" clickFunction={() => createNewLocation()}/>
+                    </Col>
+                </>
+                }
+            </Row>
+        </Container>
     );
 };
