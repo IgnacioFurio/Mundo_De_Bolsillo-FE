5741b4584e554492ef2d4c6ffc341f23b425f4fa
Ignacio Furió José <bichoifj@gmail.com>
2024-07-25 16:54:58 +0200

    created WoodenButton at the end of the form to send the newdata to the data base

--- src/pages/NewKnowledge/NewKnowledge.jsx
+++ src/pages/NewKnowledge/NewKnowledge.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
+import { validate } from '../../helpers/validations.helper';
+import { NextPrevButton } from '../../common/NextPrevButton/NextPrevButton';
+import { WoodenButton } from '../../common/WoodenButton/WoodenButton';
+import { useSelector } from 'react-redux';
+import { gameData } from '../../services/game.slice';
 
 export const NewKnowledge = () => {
+    const gameRdx = useSelector(gameData);
+
+    const [ worlds, setWorlds ] = useState([]);
 
     //HOOKS
     const [ placeholder, setPlaceholder ] = useState({
@@ -31,7 +39,7 @@ export const NewKnowledge = () => {
         heard_from_character_idValid: true,
         about_location_idValid: true,
         heard_on_location_idValid: true,
-        veracityValid: false,
+        veracityValid: true,
     });
     
     const [ errorInputField, setErrorInputField ] = useState({
@@ -41,20 +49,71 @@ export const NewKnowledge = () => {
         heard_from_character_idError: true,
         about_location_idError: true,
         heard_on_location_idError: true,
-        veracityError: false,
+        veracityError: true,
     });
 
     const [ submitStatus, setSubmitStatus ] = useState(false);
+
+    //USEEFFECT
+    useEffect(() => { 
+        console.log(gameRdx.gameInformation.id);
+
+    }, []);
+
+    // useEffect(() => { console.log(newKnowledgeData); }, [newKnowledgeData]);
+    // useEffect(() => { console.log(submitStatus); }, [submitStatus]);
+    useEffect(() => { setSubmitStatus(checkSubmitStatus()); }, [validInputField]);
+
+    //HANDLERS
+    const inputHandler = (e) => {  
+        setNewKnowledgeData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value
+        }));
+
+        checkError(e);
+    };
+
+    //CHECKS
+    const checkError = (e) => {
+        let error = "";
+    
+        let check = validate(
+            e.target.name,
+            e.target.value,
+            e.target.required
+            );
+            
+        error = check.message;
+
+        setValidInputField((prevState) => ({
+            ...prevState,
+            [e.target.name + 'Valid']: check.valid
+        }));
+        
+        setErrorInputField((prevState) => ({
+            ...prevState,
+            [e.target.name + 'Error']: error
+        }));
+    };
+
+    const checkSubmitStatus = () => {
+        for (const key in validInputField) {
+            if (validInputField[key] === false) { return false };
+        };
+
+        return true;
+    };
 
     return (
         <Container className='detailsBackground border border-black rounded pt-3'>
             <Row className='text-center'>
                 <Col className='bannerRibbon fw-bold d-flex justify-content-center align-items-center pb-3'>
                     <input 
-                        className='rounded col-9'
+                        className='col-9 text-center rounded'
                         name="title"
                         required={true}
-                        onChange={() => {}}/>
+                        onChange={(e) => inputHandler(e)}/>
                 </Col>
             </Row>
             <Row className='borderDataCard centerScrollLocations d-flex border border-black justify-content-start align-items-center py-1 mx-2'>                            
@@ -75,8 +134,17 @@ export const NewKnowledge = () => {
             </Row>
             <Row className='text-center my-2'>
                 <Col className='col-1'/>
-                <input className='col-10 rounded'></input>
+                <input 
+                    className='col-10 text-center rounded'
+                    name="description"
+                    required={false}
+                    onChange={(e) => inputHandler(e)}/>
                 <Col className='col-1'/>
+            </Row>
+            <Row className='d-flex justify-content-center  my-2'>
+                <Col className='col-4'>
+                    <WoodenButton action="send" clickFunction={() => {}}/>
+                </Col>
             </Row>
         </Container>
     )
