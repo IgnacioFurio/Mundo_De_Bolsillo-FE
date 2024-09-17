f92b6b0045ca2bf511ed8c8b720652c5f727d46d
Ignacio Furió José <bichoifj@gmail.com>
2024-09-17 18:29:38 +0200

    style: created different styles when a quest is completed or not

--- src/common/QuestCard/QuestCard.jsx
+++ src/common/QuestCard/QuestCard.jsx
@@ -13,19 +13,31 @@ export const QuestCard = ({ characterQuestData }) => {
     // const knowledgeRdx = useSelector(knowledgeData);
 
     //HOOKS
-    const [ quests, setQuests ] = useState();
+    const [ quest, setQuest ] = useState();
 
     const [ showMoreData, setShowMoreData ] = useState(false);
 
+    const [ questCardDesign, setQuestCardDesign ] = useState();
+
+    const [ titleCardDesign, setTitleCardDesign ] = useState();
+
     //USEEFFECT
     useEffect(() => { 
-        console.log(quests?.quest?.name);
-        
-        setQuests(characterQuestData); }, [quests]);
+        setQuest(characterQuestData)
+        }, []);
+
+    useEffect(() => {
+        questStatusHandler(quest);
+    }, [quest]);
 
     //HANDLER
     const showMoreHandler = () => {
         showMoreData === false ? setShowMoreData(true) : setShowMoreData(false);
+    };
+
+    const questStatusHandler = () => {
+        quest?.quest?.status === true ? setQuestCardDesign("QuestCardShadow text-center") : setQuestCardDesign("QuestCardShadowComplete text-center")
+        quest?.quest?.status === true ? setTitleCardDesign("bannerRibbonQuest fw-bold py-2") : setTitleCardDesign("bannerRibbonQuestComplete fw-bold py-2")
     };
 
     // CREAR SLICER PARA LAS MISIONES
@@ -36,9 +48,9 @@ export const QuestCard = ({ characterQuestData }) => {
 
     return (
         <Container className='mt-3'>
-            <Row className='QuestCardShadow text-center' onClick={(e) => knowledgeHandler(e)}>
-                <Col className='bannerRibbonQuest fw-bold py-2'>
-                    {quests?.quest?.name}
+            <Row className={questCardDesign} onClick={() => {}}>
+                <Col className={titleCardDesign}>
+                    {quest?.quest?.name}
                 </Col>
             </Row>
             <Row className='text-start'>                    
@@ -47,20 +59,20 @@ export const QuestCard = ({ characterQuestData }) => {
                 <Container className='centerScrollLocations col-11 mt-1'>
                     <Row className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'>                            
                         <Col className='heardFromCharacterIcon col-2 fw-bold text-center'></Col>
-                        <Col className='col-10'>{quests?.quest?.delievered_by_character_id || "??"}</Col>
+                        <Col className='col-10'>{quest?.quest?.delievered_by_character_id || "??"}</Col>
                     </Row>
                     <Row className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'>                            
                         <Col className='heardOnLocationIcon col-2 fw-bold text-center'></Col>
-                        <Col className='col-10'>{quests?.quest?.got_in_location_id || "??"}</Col>
+                        <Col className='col-10'>{quest?.quest?.got_in_location_id || "??"}</Col>
                     </Row>
                     <Row className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'>                            
                         <Col className='locationIcon col-2 fw-bold text-center'></Col>
-                        <Col className='col-10'>{quests?.quest?.happens_in_location_id || "??"}</Col>
+                        <Col className='col-10'>{quest?.quest?.happens_in_location_id || "??"}</Col>
                     </Row>
                 </Container>
                 <Container>
                     <Row className='text-center my-1'>
-                        <Col className='col-12 mb-1'>{quests?.quest?.goal}</Col>                            
+                        <Col className='col-12 mb-1'>{quest?.quest?.goal}</Col>                            
                     </Row>
                 </Container>
                 </>
