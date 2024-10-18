6e1a1f820a837d9db7cfac7cc3f64bd1e029c94a
Ignacio Furió José <bichoifj@gmail.com>
2024-10-18 12:50:47 +0200

    feat: dropDown handler created, drop`down formulary created

--- src/pages/ModifyQuest/ModifyQuest.jsx
+++ src/pages/ModifyQuest/ModifyQuest.jsx
@@ -6,25 +6,50 @@ import { getWorldGatesByGameId } from '../../services/worldgate.apicall';
 import { getLocationsByWorldId } from '../../services/location.apicalls';
 import { getCharactersByWorldId } from '../../services/character.apicalls';
 import { validate } from '../../helpers/validations.helper';
+import { WoodenButton } from '../../common/WoodenButton/WoodenButton';
+import { useNavigate } from 'react-router-dom';
 
 export const ModifyQuest = () => {
+    const navigate = useNavigate();
+
     const questRdx = useSelector((state) => state.quest);
     const gameRdx = useSelector(gameData);
 
     const [ questInformation, setQuestInformation ] = useState({
-        name: "",
-        goal: "",
-        delievered_by_character_id: null,
-        got_in_location_id: null,
-        happens_in_location_id: null,
+        name: questRdx?.questInformation?.name,
+        goal:  questRdx?.questInformation?.goal,
+        delievered_by_character_id:  questRdx?.questInformation?.delievered_by_character_id,
+        got_in_location_id:  questRdx?.questInformation?.got_in_location_id,
+        happens_in_location_id:  questRdx?.questInformation?.happens_in_location_id,
         characters_id: [],
         status: true /*Predefinimos el estado de la misión como activa*/
     });
 
+    const [ validInputField, setValidInputfield ] = useState({
+        nameValid: false,    /*Establecemos como falso los valores que son obligatorios */
+        goalValid: true,
+        delievered_by_character_idValid: true,
+        got_in_location_idValid: true,
+        happens_in_location_idValid: true,
+        characters_idValid: true,
+        statusValid: true
+    });
+    
+    const [ errorInputField, setErrorInputfield ] = useState({
+        nameError: "",
+        goalError: "",
+        delievered_by_character_idError: "",
+        got_in_location_idError: "",
+        happens_in_location_idError: "",
+        characters_idError: "",
+        status: ""
+    });
+
     const [ worlds, setWorlds ] = useState([]);
     const [ characters, setCharacters ] = useState([]);
-    const [ charactersQuest, setCharactersQuest ] = useState([]);
     const [ locations, setLocations ] = useState([]);
+
+    const [ submitStatus, setSubmitStatus ] = useState(false);
 
     useEffect(() => {console.log(questRdx.questInformation);
     }, []);
@@ -46,6 +71,16 @@ export const ModifyQuest = () => {
         setQuestInformation((prevState) => ({
             ...prevState,
             [e.target.name]: e.target.value
+        }));
+
+        checkError(e);
+};
+
+    //handler para el dropdown del formulario
+    const dropdownHandler = (e) => {       
+        setQuestInformation((prevState) => ({
+            ...prevState,
+            [e.target.name]: parseInt(e.target.value)
         }));
 
         checkError(e);
@@ -138,6 +173,97 @@ export const ModifyQuest = () => {
                         required={true}
                         placeholder={questRdx?.questInformation?.name || "Título"}
                         onChange={(e) => inputHandler(e)}/>
+                </Col>
+            </Row>
+            <Row className='text-start'>                    
+                <Container className='centerScrollLocations col-11 mt-1'>
+                    <Row className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'>                            
+                        <Col className='heardFromCharacterIcon col-2 fw-bold text-center'></Col>
+                        <Col className='col-10'>
+                            <select 
+                                className='col-12 rounded'
+                                name={"delievered_by_character_id"} 
+                                onChange={(e) => dropdownHandler(e)}
+                                >
+                                <option 
+                                    value={questRdx?.questInformation?.delieveredByCharacter?.id} 
+                                    label={`Contado por...: ${questRdx?.questInformation?.delieveredByCharacter?.name}`}
+                                />
+                                {!characters ? ( 
+                                        <></>
+                                    ) : (
+                                    characters.map((data) => { 
+                                        return  <option
+                                            key={data.id}
+                                            value={data.id}
+                                            label={data.name}
+                                            >
+                                                {data.name}
+                                            </option>
+                                }))}
+                            </select>
+                        </Col>
+                    </Row>
+                    <Row className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'>                            
+                        <Col className='heardOnLocationIcon col-2 fw-bold text-center'></Col>
+                        <Col className='col-10'>
+                            <select 
+                                className='col-12 rounded'
+                                name={"got_in_location_id"} 
+                                onChange={(e) => dropdownHandler(e)}
+                                >
+                                <option 
+                                    value={questRdx?.questInformation?.gotInLocation?.id} 
+                                    label={`Escuchado en: ${questRdx?.questInformation.gotInLocation.name}` }
+                                    />
+                                {!locations ? ( 
+                                        <></>
+                                    ) : (
+                                    locations.map((data) => { 
+                                        return  <option
+                                            key={data.id}
+                                            value={data.id}
+                                            label={data.name}
+                                            >
+                                                {data.name}
+                                            </option>
+                                }))}
+                            </select>
+                        </Col>
+                    </Row>
+                    <Row className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'>                            
+                        <Col className='locationIcon col-2 fw-bold text-center'></Col>
+                        <Col className='col-10'> 
+                        <select 
+                                className='col-12 rounded'
+                                name={"happens_in_location_id"} 
+                                onChange={(e) => dropdownHandler(e)}
+                                >
+                                <option 
+                                    value={questRdx?.questInformation?.happensInLocation?.id} 
+                                    label={`Ocurre en: ${questRdx?.questInformation?.happensInLocation?.name}`}
+                                    />
+                                {!locations ? ( 
+                                        <></>
+                                    ) : (
+                                    locations.map((data) => { 
+                                        return  <option
+                                            key={data.id}
+                                            value={data.id}
+                                            label={data.name}
+                                            >
+                                                {data.name}
+                                            </option>
+                                }))}
+                            </select>
+                        </Col>
+                    </Row>
+                </Container>
+            </Row>
+            <Row>
+                <Col className='col-12 d-flex justify-content-evenly py-3'>
+                    <WoodenButton activateButton={true} action="back" clickFunction={() => navigate("/quests/quest-details")}/>
+                    <WoodenButton activateButton={submitStatus} action="submit" clickFunction={() => {}}/>
                 </Col>
             </Row>
         </Container>
