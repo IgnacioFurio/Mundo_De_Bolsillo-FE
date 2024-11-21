42d6d1dcd6f391a90f740e03397f732c03e41873
Ignacio Furió José <bichoifj@gmail.com>
2024-11-21 19:48:17 +0100

    feat: scenes are shown sorted by their session_index at ModifySession

--- src/pages/ModifySession/ModifySession.jsx
+++ src/pages/ModifySession/ModifySession.jsx
@@ -1,7 +1,157 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Col, Container, Row } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { sessionData } from '../../services/session.slice';
+import { SearchBar } from '../../common/SearchBar/SearchBar';
+import { DraggableSceneCard } from '../../common/DraggableSceneCard/DraggableSceneCard';
+import { WoodenButton } from '../../common/WoodenButton/WoodenButton';
 
 export const ModifySession = () => {
+    const navigate = useNavigate();
+    const sessionRdx = useSelector(sessionData);
+    
+    const [ session, setSession ] = useState(
+        {
+            title: sessionRdx?.sessionInformation?.title,
+            description: sessionRdx?.sessionInformation?.description,
+            game_id: sessionRdx?.sessionInformation?.id,
+            scenesAtSession: sessionRdx?.sessionInformation?.scenesAtSession,
+        }
+    );
+
+    const [ validInputField, setValidInputField ] = useState(
+        {   //valor en falso para los requeridos
+            titleValid: false,
+            descriptionValid: true,
+            game_idValid: true,
+        }
+    );
+
+    const [ errorInputField, setErrorInputfield ] = useState(
+        {
+            titleError: "",
+            descriptionError: "",
+            game_idError: "",
+        }
+    );
+
+    const [ scenes, setScenes ] = useState([]);
+    const [ scenesAtSession, setScenesAtSession ] = useState([]);
+
+    const [ searchInput, setSearchInput ] = useState("");
+    const [ searchResult, setSearchResult ] = useState([]);
+
+    const [ submitStatus, setSubmitStatus ] = useState(false);
+
+    useEffect(() => { sortOff(session?.scenesAtSession); }, [sessionRdx]);
+
+    useEffect(() => { console.log();
+    }, [session]);
+
+    //FUNCTIONS
+    const sortOff = (arr) => {        
+        const sortArr = [...arr].sort((a,b) => a.session_index - b.session_index);
+        
+        setSession((prevState) => (
+            {
+                ...prevState,
+                scenesAtSession: sortArr
+            }
+        ));
+    };
+
     return (
-        <div>ModifySession</div>
-    )
+        <Container className='col-12 col-sm-11 col-md-8 pb-2'>
+            <Row className='upperScroll d-flex justify-content-center align-items-center' >
+                <input 
+                    className='col-9 QuestCardShadow fw-bold fs-5 text-center eb-garamond-font rounded ms-4'
+                    name="title"
+                    required={true}
+                    value={session.title}
+                    placeholder={"¿Cómo llamarás a la sesión?"}
+                    onChange={(e) => inputHandler(e)}
+                    />
+            </Row>
+            <Container className='centerScrollLocations col-10'>
+                <Row 
+                    className='borderDataCard d-flex border border-black justify-content-start align-items-center py-1 px-2'
+                    droppable="true">                            
+                    <Col className='col-12 my-1 text-center fw-bold'>Escenas</Col>
+                    <Col 
+                        className='col-12 py-2 rounded' 
+                        droppabe="true" 
+                        style={{
+                            border: '1px solid #ddd',
+                            cursor: 'move',
+                            alignContent: "center"
+                        }}>
+                            {!session?.scenesAtSession ? (
+                                    <></>
+                                ) : (
+                                    session?.scenesAtSession.map((data, index) => {
+                                        return <button 
+                                            key={data.id}
+                                            draggable="true"
+                                            className='col-12 d-flex justify-content-evenly align-items-center rounded my-1'
+                                            onClick={(e) => setScenesForSession(e, data.id, "scenesAtSession")}
+                                            onDragStart={(e) => startDrag(e, index)}
+                                            onDragOver={(e) => draggingOver(e)}
+                                            onDrop={(e) => onDrop(e, index)}>
+                                            {index}{" "}{data.title}
+                                        </button>
+                                    })
+                                )
+                            }
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='col-12'>
+                        <SearchBar
+                            className="col-12 rounded" 
+                            onChangeFunction={(e) => shearchBarHandler(e)}
+                            placeholder={"¿Qué escena buscas?"}/>
+                        {!searchInput ? (
+                                scenes.map((data) => {
+                                    return <DraggableSceneCard 
+                                        key={data.id}
+                                        sceneData={data} 
+                                        onClickFunction={(e) => setScenesForSession(e, data.id, "scenes")}
+                                        scenes={scenes}/>
+                                })
+                            ) : (
+                                searchResult.map((data) => {
+                                    return <DraggableSceneCard 
+                                        key={data.id}
+                                        sceneData={data} 
+                                        onClickFunction={(e) => setScenesForSession(e, data.id, "scenes")}
+                                        scenes={scenes}/>
+                                })
+                        )}
+                    </Col>
+                </Row>
+                <Row className='text-center py-1'>
+                    <Col className='col-12 mt-1 '> 
+                        <textarea 
+                            className='col-11 text-center rounded'
+                            name="description"
+                            required={false}
+                            value={session.description}
+                            placeholder={"Resumen de ha de ocurrir en la sesión"}
+                            onChange={(e) => inputHandler(e)}
+                            style={{height: 8 + "em"}}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className='col-12 d-flex justify-content-evenly py-3'>
+                        <WoodenButton activateButton={true} action="back" clickFunction={() => navigate("/games/game-details")}/>
+                        <WoodenButton activateButton={submitStatus} action="submit" clickFunction={() => createNewSession()}/>
+                    </Col>
+                </Row>
+            </Container> 
+            <Row className='downScroll d-flex justify-content-center align-items-center'>
+                <Col className='col-12 text-center fw-bold'>{}</Col>
+            </Row>
+        </Container>
+    );
 }
