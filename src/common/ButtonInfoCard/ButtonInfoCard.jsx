34ed442cf5d9603ef2fc8b0307877c44191d6083
Ignacio Furió José <bichoifj@gmail.com>
2024-12-10 18:29:50 +0100

    feat: onclick button now shows more info about location

--- src/common/ButtonInfoCard/ButtonInfoCard.jsx
+++ src/common/ButtonInfoCard/ButtonInfoCard.jsx
@@ -1,14 +1,21 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import Modal from 'react-bootstrap/Modal';
 import { WoodenButton } from '../WoodenButton/WoodenButton';
 
 
-export const ButtonInfoCard = ({ infoCard }) => {
+export const ButtonInfoCard = ({ infoCard, source }) => {
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    const [ sourceType, setSourceType ] = useState(source);
+
+    useEffect(() => {
+        console.log(infoCard);
+        
+    },[]);
 
     return (
         <>
@@ -22,6 +29,32 @@ export const ButtonInfoCard = ({ infoCard }) => {
                     <Col className='bannerRibbon text-center fw-bold py-2'>
                         {infoCard?.name || infoCard?.title}
                     </Col>
+                    {source === "location" ? (
+                        <Container className='centerScrollLocations col-10 '>
+                            <Row className='borderDataCard d-flex justify-content-start align-items-center py-1 px-2'>                            
+                                <Col className='governmentIcon col-1 fw-bold text-center'></Col>
+                                <Col className='col-9'>{infoCard.government}</Col>
+                            </Row>
+                            <Row className='borderDataCard d-flex justify-content-start align-items-center mt-0 py-1 px-2'>                            
+                                <Col className='defensesIcon col-1 fw-bold'></Col>
+                                <Col className='col-10'> {infoCard.defenses}</Col>
+                            </Row>
+                            <Row className='borderDataCard d-flex justify-content-start align-items-center mt-0 py-1 px-2'>
+                                <Col className='commerceIcon col-1 fw-bold'></Col>
+                                <Col className='col-10'> {infoCard.commerce}</Col>
+                            </Row>
+                            <Row className='borderDataCard d-flex justify-content-start align-items-center mt-0 py-1 px-2'>
+                                <Col className='populationIcon col-1 fw-bold'></Col>
+                                <Col className='col-10'> {infoCard.population}</Col>
+                            </Row>
+                            <Row className='borderDataCard d-flex justify-content-start align-items-center mt-0 py-1 px-2'>
+                                <Col className='worldIcon col-1 fw-bold text-center'></Col>
+                                <Col className='col-10'>{infoCard?.World?.name || "??"}</Col>
+                            </Row>
+                        </Container>
+                    ) : (
+                        <></>
+                    )}
                     <Col className='col-12 d-flex justify-content-evenly py-3'>
                         <WoodenButton activateButton={true} action="back" clickFunction={() => handleClose()}/>
                         <WoodenButton activateButton={true} action="submit" clickFunction={() => {}}/>
